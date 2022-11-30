5bf1681fad02384cbda6d2965bf39a79da5b22c9
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-29 22:41:21 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.querySelector('form')
+form.addEventListener('submit',(e) =>{
+  e.preventDefault()
+  handleInfo(e.target['new-task-description'].value)
+})
+
+const handleInfo = (text) => {
+const paragraph = document.createElement('p')
+paragraph.textContent= `${text}`
+document.querySelector('#tasks').append(paragraph)
+} 
+
 });
+
+// function removeTasks() {
+//   document.removeEventById('list')\
+//   }
